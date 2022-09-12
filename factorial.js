2c26096db4874d33bd87e9de4172ed3c6393d961
Alamgir kabir <alamgir.saguli@gmail.com>
2022-09-12 09:41:38 +0600

    3rd commit

--- factorial.js
+++ factorial.js
@@ -1,6 +1,15 @@
 // 4!= 4*3*2*1
-let factorial = 1;
-for(let i= 4; i>1; i--){
-    factorial = factorial * i;
+// let factorial = 1;
+// for(let i= 4; i>1; i--){
+//     factorial = factorial * i;
+// }
+// console.log(factorial);
+
+function factorial(i){
+    if(i==1){
+        return 1;
+    }
+    return i * factorial(--i);
 }
-console.log(factorial);
+console.log(factorial(4));
+// class 23_5-3 end.
